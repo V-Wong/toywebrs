e0bde726ceb9b5fbe7a6efc4f00bcdf313bb88b5
Vincent Wong <vincent@vwong.dev>
2024-11-05 21:45:14 +1100

    refactor(library): Split library and binary

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
-mod request;
-mod response;
-mod router;
-
 use std::{collections::HashMap, fs, net::TcpListener};
 
-use request::Method;
-use response::Response;
-use router::Router;
+use toywebrs::request::Method;
+use toywebrs::response::{Response, Status};
+use toywebrs::router::Router;
 
 fn main() {
     let listener = TcpListener::bind("localhost:8080").unwrap();
     Router::new(listener)
         .add_route(Method::GET, "/", |_| Response {
-            status: response::Status::Ok,
+            status: Status::Ok,
             headers: HashMap::new(),
             body: Some(fs::read_to_string("assets/hello.html").unwrap()),
         })
         .add_route(Method::GET, "/other", |_| Response {
-            status: response::Status::NotFound,
+            status: Status::NotFound,
             headers: HashMap::new(),
             body: Some(fs::read_to_string("assets/404.html").unwrap()),
         })
