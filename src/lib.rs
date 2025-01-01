66374b1efea7f699367e58b3281ffd6ec833d3ee
Vincent Wong <vincent@vwong.dev>
2025-01-02 00:23:05 +1100

    refactor(http): Create dedicated HTTP module

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 pub mod prelude {
-    pub use crate::request::{Method, Request};
-    pub use crate::response::{Response, Status};
+    pub use crate::http::request::{Method, Request};
+    pub use crate::http::response::{Response, Status};
     pub use crate::router::Router;
     pub use crate::thread_pool::ThreadPool;
 }
 
-mod request;
-mod response;
+mod http;
 mod router;
 mod thread_pool;
