1ffd7f55f081704e3115040a701244ce5b206120
Vincent Wong <vincent@vwong.dev>
2025-01-02 22:56:03 +1100

    refactor(headers): Convert `Headers` to tuple struct

--- src/http/headers.rs
+++ src/http/headers.rs
@@ -1,46 +1,41 @@
 use std::collections::HashMap;
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct Headers {
-    fields: HashMap<String, String>,
-}
+pub struct Headers(HashMap<String, String>);
 
 impl Headers {
     pub fn new() -> Self {
-        Self {
-            fields: HashMap::new(),
-        }
+        Self(HashMap::new())
     }
 
     pub fn add(&mut self, key: &str, value: &str) -> &mut Self {
-        self.fields.insert(key.to_string(), value.to_string());
+        self.0.insert(key.to_string(), value.to_string());
         self
     }
 }
 
 impl From<HashMap<String, String>> for Headers {
     fn from(value: HashMap<String, String>) -> Self {
-        Headers { fields: value }
+        Self(value)
     }
 }
 
 impl FromIterator<String> for Headers {
     fn from_iter<T: IntoIterator<Item = String>>(iter: T) -> Self {
-        Headers {
-            fields: iter
-                .into_iter()
+        Headers(
+            iter.into_iter()
                 .flat_map(|header| {
                     let (key, value) = header.split_once(":")?;
                     Some((key.to_string(), value.trim_start().to_string()))
                 })
                 .collect(),
-        }
+        )
     }
 }
 
 impl ToString for Headers {
     fn to_string(&self) -> String {
-        self.fields
+        self.0
             .iter()
             .map(|(key, value)| format!("{key}: {value}\r\n"))
             .collect::<String>()
