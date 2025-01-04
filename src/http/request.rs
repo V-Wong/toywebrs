836188bda172073145c9c1f881c63580c401c932
Vincent Wong <vincent@vwong.dev>
2025-01-04 20:21:13 +1100

    feat(request): Add parsing of HTTP request body

--- src/http/request.rs
+++ src/http/request.rs
@@ -10,6 +10,7 @@ pub struct Request {
     pub method: Method,
     pub path: String,
     pub headers: Headers,
+    pub body: Option<String>,
 }
 
 #[derive(Debug)]
@@ -23,22 +24,20 @@ impl TryFrom<&mut dyn Read> for Request {
     type Error = RequestParsingError;
 
     fn try_from(value: &mut dyn Read) -> Result<Self, RequestParsingError> {
-        let mut lines = BufReader::new(value)
-            .lines()
-            .map(|result| result.unwrap())
-            .take_while(|line| !line.is_empty());
+        let mut lines = BufReader::new(value).lines().map(|result| result.unwrap());
 
         let RequestLine(method, path) = lines
             .next()
             .ok_or(RequestParsingError::NoRequestLine)?
             .parse()?;
-
-        let headers = lines.by_ref().take_while(|line| line != CRLF).collect();
+        let headers = lines.by_ref().take_while(|line| !line.is_empty()).collect();
+        let body = lines.reduce(|acc, line| acc + CRLF + &line);
 
         Ok(Request {
             method,
             path,
             headers,
+            body,
         })
     }
 }
@@ -67,6 +66,7 @@ impl FromStr for RequestLine {
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum Method {
     GET,
+    POST,
 }
 
 impl FromStr for Method {
@@ -75,6 +75,7 @@ impl FromStr for Method {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "GET" => Ok(Method::GET),
+            "POST" => Ok(Method::POST),
             _ => Err(()),
         }
     }
@@ -88,7 +89,8 @@ mod tests {
 
     #[test]
     fn can_parse_get_request() {
-        let mut message = "GET /test.htm HTTP/1.1
+        let mut message = "\
+GET /test.htm HTTP/1.1
 User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
 Host: www.vwong.dev
 Accept-Language: en-uk
@@ -111,7 +113,43 @@ Connection: Keep-Alive"
                     ("Accept-Encoding".into(), "gzip, deflate".into()),
                     ("Connection".into(), "Keep-Alive".into())
                 ])
-                .into()
+                .into(),
+                body: None
+            }
+        )
+    }
+
+    #[test]
+    fn can_parse_post_request() {
+        let mut message = "\
+POST /test.htm HTTP/1.1
+User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
+Host: www.vwong.dev
+Accept-Language: en-uk
+Accept-Encoding: gzip, deflate
+Connection: Keep-Alive
+
+FIRST BODY LINE
+SECOND BODY LINE"
+            .as_bytes();
+
+        assert_eq!(
+            Request::try_from(&mut message as &mut dyn Read).unwrap(),
+            Request {
+                method: Method::POST,
+                path: "/test.htm".into(),
+                headers: HashMap::from([
+                    (
+                        "User-Agent".into(),
+                        "Mozilla/4.0 (compatible; MSIE5.01; Windows NT)".into()
+                    ),
+                    ("Host".into(), "www.vwong.dev".into()),
+                    ("Accept-Language".into(), "en-uk".into()),
+                    ("Accept-Encoding".into(), "gzip, deflate".into()),
+                    ("Connection".into(), "Keep-Alive".into())
+                ])
+                .into(),
+                body: Some(format!("FIRST BODY LINE{CRLF}SECOND BODY LINE"))
             }
         )
     }
