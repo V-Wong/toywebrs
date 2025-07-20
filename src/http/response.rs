caa2c99b4074d1a8bb0606147a01e930d8002b29
Vincent Wong <vincent@vwong.dev>
2025-07-20 22:31:53 +1000

    feat(response): Add remaining HTTP status codes

--- src/http/response.rs
+++ src/http/response.rs
@@ -23,14 +23,34 @@ impl From<Response> for String {
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Status {
     Ok = 200,
+    Created = 201,
+    NoContent = 204,
+    BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
     NotFound = 404,
+    MethodNotAllowed = 405,
+    InternalServerError = 500,
+    NotImplemented = 501,
+    BadGateway = 502,
+    ServiceUnavailable = 503,
 }
 
 impl Display for Status {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(match self {
             Status::Ok => "200 OK",
-            Status::NotFound => "404 Not found",
+            Status::Created => "201 Created",
+            Status::NoContent => "204 No Content",
+            Status::BadRequest => "400 Bad Request",
+            Status::Unauthorized => "401 Unauthorized",
+            Status::Forbidden => "403 Forbidden",
+            Status::NotFound => "404 Not Found",
+            Status::MethodNotAllowed => "405 Method Not Allowed",
+            Status::InternalServerError => "500 Internal Server Error",
+            Status::NotImplemented => "501 Not Implemented",
+            Status::BadGateway => "502 Bad Gateway",
+            Status::ServiceUnavailable => "503 Service Unavailable",
         })
     }
 }
